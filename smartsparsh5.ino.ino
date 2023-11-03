0c994265f0b1096ff2601597af46d5a553944a26
Abhisekhkuma <149614984+Abhisekhkuma@users.noreply.github.com>
2023-11-03 09:48:25 +0530

    Update smartsparsh5.ino

--- smartsparsh5.ino.ino
+++ smartsparsh5.ino.ino
@@ -42,51 +42,6 @@ lcd.print("                ");
 if(A==0 &&B==0 && C==1 && D==0)
 {
     lcd.setCursor(0, 1);
-  lcd.p…
-[4:43 pm, 02/11/2023] It Is Me: #include <Wire.h>
-
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,16,2);
-void setup() {
-
-  lcd.init();
-  lcd.backlight();
-lcd.setCursor(0, 0);  
-  lcd.print(" ACCESS  DENIED ");
-  
-  pinMode(3,INPUT);
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
-  pinMode(6,INPUT);
-  
-}
-
-void loop() {
-int A=digitalRead(3);
-int B=digitalRead(4);
-int C=digitalRead(5);
-int D=digitalRead(6);
-
-if(A==1 &&B==0 && C==0 && D==0)
-{
-    lcd.setCursor(0, 1);
-   lcd.print(" HELLO ");
-delay (3000);
-    lcd.setCursor(0, 1);
-lcd.print("                ");
-}
-else if(A==0 &&B==1 && C==0 && D==0)
-{
-    lcd.setCursor(0, 1);
-  lcd.print("I AM HUNGRY");
-delay (3000);
-    lcd.setCursor(0, 1);
-lcd.print("                ");
-}
-if(A==0 &&B==0 && C==1 && D==0)
-{
-    lcd.setCursor(0, 1);
   lcd.print("I WANT TO BATH");
 delay (3000);
     lcd.setCursor(0, 1);
